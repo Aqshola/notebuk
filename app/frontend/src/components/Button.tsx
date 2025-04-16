7e7171c8058fa5d851055b54b008bba686507cc6
Aqshola <aqshola.afid@gmail.com>
2025-04-17 00:17:44 +0700

    feat: change page transition

--- app/frontend/src/components/Button.tsx
+++ app/frontend/src/components/Button.tsx
@@ -113,7 +113,6 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
       renderDrawHandDrawn();
 
       const resizeObserver = new ResizeObserver(() => {
-        console.log("didi");
         renderDrawHandDrawn();
       });
 
--- app/frontend/src/components/custom/Transition.tsx
+++ app/frontend/src/components/custom/Transition.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { SwitchTransition, Transition } from "react-transition-group";
 import { useLocation } from "react-router-dom";
 import gsap from "gsap";
@@ -10,34 +10,59 @@ interface Props {
 const TransitionComponent = ({ children }: Props) => {
   const location = useLocation();
   const { setComplete } = useTransitionStore((state) => state);
+  const bgRef = useRef<HTMLDivElement>(null);
+  const [positionBg, setPositionBg] = useState({
+    start: "-100%",
+    end: "100%",
+  });
+
   return (
-    <SwitchTransition>
-      <Transition
-        key={location.pathname}
-        timeout={500}
-        onEnter={(node: any) => {
-          setComplete(false);
-          gsap.set(node, { autoAlpha: 0, xPercent: -100 });
-          gsap
-            .timeline({
-              paused: true,
-              onComplete: () => setComplete(true),
-            })
-            .to(node, { autoAlpha: 1, xPercent: 0, duration: 0.25 })
-            .to(node, { duration: 0.25 })
-            .play();
-        }}
-        onExit={(node) => {
-          gsap
-            .timeline({ paused: true })
-            .to(node, { duration: 0.2 })
-            .to(node, { xPercent: 100, autoAlpha: 0, duration: 0.2 })
-            .play();
-        }}
-      >
-        {children}
-      </Transition>
-    </SwitchTransition>
+    <>
+      <div
+        ref={bgRef}
+        className="bg-primary-purple fixed -translate-x-[100%] z-50 w-screen h-screen top-0"
+      ></div>
+      <SwitchTransition>
+        <Transition
+          key={location.pathname}
+          timeout={500}
+          onEnter={(node: any) => {
+            gsap
+              .timeline({
+                paused: true,
+                onComplete: () => {
+                  setComplete(true);
+
+                  if (positionBg.end == "100%") {
+                    setPositionBg({
+                      end: "-100%",
+                      start: "100%",
+                    });
+                  } else {
+                    setPositionBg({
+                      start: "-100%",
+                      end: "100%",
+                    });
+                  }
+                },
+              })
+              .to(bgRef.current, { translateX: positionBg.end })
+              .to(node, { opacity: 100, duration: 0.3 })
+              .play();
+          }}
+          onExit={(node) => {
+            gsap.set(bgRef.current, { translateX: positionBg.start });
+            gsap
+              .timeline({ paused: true })
+              .to(bgRef.current, { translateX: "0" })
+              .to(node, { opacity: 0 })
+              .play();
+          }}
+        >
+          {children}
+        </Transition>
+      </SwitchTransition>
+    </>
   );
 };
 
